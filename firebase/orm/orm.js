0910fcf0c3c7ff048b9ea02f988e1f3bfa6fefa0
pgerspach <pgerspac@alumni.nd.edu>
2019-02-02 11:16:11 -0500

    Cleaned up firebase orm and authroutes. Looks much nicer

--- firebase/orm/orm.js
+++ firebase/orm/orm.js
@@ -1,32 +1,85 @@
 module.exports = {
-    signIn: function(firebase, token) {
-      var credential = firebase.auth.GoogleAuthProvider.credential(
-        // get user credential from token provided by client
-        token
-      );
-      console.log(credential);
-      return new Promise((resolve, reject) => {
-        firebase
-          .app()
-          .auth()
-          .signInAndRetrieveDataWithCredential(credential)
-          .then(data => {
-            resolve({
-              statusCode: 200,
-              data: data
-            });
-          })
-          .catch(function(error) {
-            console.log("Here in sign in failed");
-            resolve({
-              statusCode: 404,
-              data: error
-            });
+  signInGoogle: function(firebase, token) {
+    var credential = firebase.auth.GoogleAuthProvider.credential(
+      token
+    );
+    return new Promise((resolve, reject) => {
+      firebase
+        .app()
+        .auth()
+        .signInAndRetrieveDataWithCredential(credential)
+        .then(data => {
+          resolve({
+            statusCode: 200,
+            firebase: firebase
           });
-      });
-  
-    },
-    currentUser:function(firebase){
-      return firebase.auth().currentUser;
-    }
-  };
+        })
+        .catch(function(error) {
+          reject({
+            statusCode: 404,
+            errorCODE: error.code
+          });
+        });
+    });
+  },
+  signInEmail: function(firebase, req) {
+    return new Promise((resolve, reject) => {
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(req.body.email, req.body.password)
+        .then(data => {
+          resolve({
+            statusCode: 200,
+            firebase: firebase
+          });
+        })
+        .catch(function(error) {
+          reject({
+            statusCode: 404,
+            errorCode: error.code
+          });
+        });
+    });
+  },
+  signOutAccount: function(firebase, req) {
+    return new Promise((resolve, reject) => {
+      firebase
+        .auth()
+        .signOut()
+        .then(function() {
+          resolve({
+            statusCode: 200,
+            firebase: firebase
+          });
+        })
+        .catch(function(error) {
+          reject({
+            statusCode: 404,
+            error: errorCode
+          });
+        });
+    });
+  },
+  signUpEmail: function(firebase, req) {
+    return new Promise((resolve, reject) => {
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(req.body.email, req.body.password)
+        .then(data => {
+          resolve({
+            statusCode: 200,
+            firebase: firebase
+          });
+        })
+        .catch(function(error) {
+          reject({
+            statusCode: 404,
+            errorCode: error.code
+          });
+        });
+    });
+  },
+  currentUser: function(firebase) {
+    return firebase.auth().currentUser;
+  }
+};
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,74 +1,88 @@
 module.exports = function(app, Firebase) {
   const fborm = require("../firebase/orm/orm.js");
+  Firebase.firebaseMain.auth().onAuthStateChanged(function(user) {
+    // Listen for change in auth status...
+    if (user) {
+      // User is signed in.
+      console.log("User in");
+    } else {
+      // No user is signed in.
+      console.log("User Not");
+    }
+  });
+  
   app.post("/auth/google", (req, res) => {
     console.log("Here in auth/google post request");
+    fborm
+      .signInGoogle(Firebase.firebaseMain, req.body.token)
+      .then(result => {
+        Firebase.firebaseMain = result.firebase;
+        let userId = fborm.currentUser(Firebase.firebaseMain).uid;
+        console.log(userId);
 
-    Firebase.firebaseMain.auth().onAuthStateChanged(function(user) {
-      // Listen for change in auth status...
-      if (user) {
-        // User is signed in.
-        console.log("User in");
-      } else {
-        // No user is signed in.
-        console.log("User Not");
-      }
-    });
-
-    fborm.signIn(Firebase.firebaseMain, req.body.token).then(resolve => {
-      let userId = fborm.currentUser(Firebase.firebaseMain).uid;
-      console.log(userId);
-      Firebase.firebaseMain
-        .auth()
-        .signOut()
-        .then(function() {
-          // Sign-out successful.
-          console.log("Sign-out successful!");
-          res.send("Success");
-        })
-        .catch(function(error) {
-          // An error happened.
-          console.log("There was an error");
-          res.send("Failure: "+error);
-          // if (error) throw error;
-        });
-    });
+        fborm
+          .signOutAccount(Firebase.firebaseMain, req)
+          .then(result => {
+            Firebase.firebaseMain = result.firebase;
+            console.log("Sign-out successful!");
+            res.send("Success");
+          })
+          .catch(result => {
+            res.status(result.statusCode).send(result.errorCode);
+          });
+      })
+      .catch(result => {
+        res.status(result.statusCode).send(result.errorCode);
+      });
   });
 
-  app.post("/auth/email", (req,res)=>{
+  app.post("/auth/email", (req, res) => {
     console.log("Here in auth/email post request");
-    Firebase.firebaseMain.auth().signInWithEmailAndPassword(req.body.email, req.body.password).then(()=>{
-      Firebase.firebaseMain.auth().signOut().then(function() {
-        console.log("Sign out successful");
-        // Sign-out successful.
-        res.send("Sign out successful");
-      }).catch(function(error) {
-        console.log(error);
-        res.sendStatus(404);
-      });
-    }).catch(function(error) {
-      if(error.code === "auth/user-not-found"){
-        Firebase.firebaseMain.auth().createUserWithEmailAndPassword(req.body.email, req.body.password).then((createUserResult)=>{
-          res.send("user created!" +createUserResult);
-          Firebase.firebaseMain.auth().signOut().then(function() {
-            console.log("Sign out successful");
-            // Sign-out successful.
-          }).catch(function(error) {
-            console.log(error);
-            res.sendStatus(404);
+    //fborm.signInEmail(Firebase.firebaseMain)...
+    fborm
+      .signInEmail(Firebase.firebaseMain, req)
+      .then(result => {
+        Firebase.firebaseMain = result.firebase;
+        let userId = fborm.currentUser(Firebase.firebaseMain).uid;
+        console.log(userId);
+        fborm
+          .signOutAccount(Firebase.firebaseMain, req)
+          .then(result => {
+            Firebase.firebaseMain = result.firebase;
+            console.log("Sign-out successful!");
+            res.send("Success");
+          })
+          .catch(result => {
+            res.status(result.statusCode).send(result.errorCode);
           });
-        }).catch(function(error) {
-          // Handle Errors here.
-          var errorCode = error.code;
-          var errorMessage = error.message;
-          // ...
-        });
-      }else if(error.code === "auth/invalid-email"){
-        res.send("email must be in proper format");
-      }
-       //var errorCode = error.code;
-      //var errorMessage = error.message;
-      // ...
-    });
+      })
+      .catch(result => {
+        res.status(result.statusCode).send(result.errorCode);
+      });
+  });
 
-  })
+  app.post("/auth/email/create", (req, res) => {
+    fborm
+      .signUpEmail(Firebase.firebaseMain, req)
+      .then(result => {
+        Firebase.firebaseMain = result.firebase;
+        let userId = fborm.currentUser(Firebase.firebaseMain).uid;
+        console.log(userId);
+        res.send("user created!");
+        console.log(req);
+        fborm
+          .signOutAccount(Firebase.firebaseMain, req)
+          .then(result => {
+            Firebase.firebaseMain = result.firebase;
+            console.log("Sign-out successful!");
+            res.send("Success");
+          })
+          .catch(result => {
+            res.status(result.statusCode).send(result.errorCode);
+          });
+      })
+      .catch(result => {
+        res.status(result.statusCode).send(result.errorCode);
+      });
+  });
 };
